b06fe31ad036b5322d0c09d9ba2d0c17fd4c2c61
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-10-08 23:23:58 +0530

    Update and rename index.html to App.js
    update this page to App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import Navbar from './component/navbar.js';
+import AboutUs from './pages/about.js';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <AboutUs/>
+      
+    </div>
+  );
+}
+
+export default App;
