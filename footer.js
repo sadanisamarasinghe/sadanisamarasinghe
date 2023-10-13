49c578c6377855f871132933893e525743cd2d46
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-10-13 22:53:20 +0530

    This is a footer component

---
+++ footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function footer(){
+    return (
+        
+        <footer className="fixed bottom-0 w-full bg-black text-white py-4 text-center">
+        &copy; 2023 Samarasinghe. All rights reserved.
+
+        </footer>
+        
+        
+    );
+}
+
+export default footer;
+
