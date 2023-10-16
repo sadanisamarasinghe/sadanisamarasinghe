b15063a611030a8031fc0ac6949908dd4fa32aef
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-10-16 23:03:14 +0530

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// App.js or index.js
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'; // Import your page components
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        {/* Add more routes for other pages */}
-      </Switch>
-    </Router>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
